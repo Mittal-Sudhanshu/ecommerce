9514b72e029f021d6df2b57c87493c98592a73b9
Ankit Kumar <ankitkumardos47@gmail.com>
2022-12-18 15:34:05 +0530

    added multer and cloudinary for images

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,15 +1,23 @@
 const asyncHandler = require("express-async-handler");
 const Category = require("../models/category");
+const fs=require('fs');
+const cloudinary = require("../cloudinary/cloudinary");
 
 const createCategory = asyncHandler(async (req, res) => {
+  const uploader = async (path) => await cloudinary.uploads(path, 'Images');
   const { name } = req.body;
-  if (!name) {
+  const { path } = req.file;
+  const newPath = await uploader(path);
+  const image=newPath;
+  fs.unlinkSync(path);
+  console.log(image);
+  if (!name,!image) {
     res.status(400).json( "Fields can't be empty");
     return;
   }
   try {
     const category = await Category.create({
-      name
+      name,image,
     });
     res.status(200).json(category);
   } catch (err) {
